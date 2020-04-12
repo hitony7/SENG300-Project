8302f3f6b64134bc6afee6084177ef9aca6f89f4
SeanPark <seanp1225@hotmail.ca>
2020-04-11 18:11:32 -0600

    Stuff

--- src/main/java/com/packagename/myapp/control/ManageUserController.java
+++ src/main/java/com/packagename/myapp/control/ManageUserController.java
@@ -1,6 +1,10 @@
 package com.packagename.myapp.control;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import com.packagename.myapp.Users;
+import com.packagename.myapp.model.User;
 
 public class ManageUserController {
 
@@ -13,5 +17,9 @@ public class ManageUserController {
 	public void setUserName(String userName) {
 		user.setName(userName);
 	}
+	
+	/*public ArrayList<User> getAllUsers(HashMap<Integer,User> userData) {
+		return List. userData.keySet()
+	}*/
 	
 }
