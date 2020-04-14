834ce7edb09eb3fc8938477fae22e83cea990585
AlexanderChao14 <chaoalexander45@gmail.com>
2020-04-13 19:36:43 -0600

    Finish adding the edit user function.

--- src/main/java/com/packagename/myapp/ManageUser.java
+++ src/main/java/com/packagename/myapp/ManageUser.java
@@ -23,6 +23,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -35,7 +36,7 @@ public class ManageUser extends VerticalLayout {
 
 	public ManageUser() {
 
-		Binder<ManageUserController> binder = new Binder(ManageUserController.class);
+		Binder<User> binder = new Binder(ManageUserController.class);
 
 		//Make a form layout for the adding user
 		FormLayout form = new FormLayout();
@@ -56,7 +57,7 @@ public class ManageUser extends VerticalLayout {
 		//creates the binder to get data easier from the user.json file
 		binder.forField(userName)
 				.asRequired()
-				.bind(ManageUserController::getUserName, ManageUserController::setUserName);
+				.bind(User::getName, User::setName);
 
 
 		
@@ -65,7 +66,7 @@ public class ManageUser extends VerticalLayout {
 
 		binder.forField(password)
 				.asRequired()
-				.bind(ManageUserController::getPassword, ManageUserController::setPassword);
+				.bind(User::getPassword, User::setPassword);
 		
 		
 		
@@ -76,28 +77,28 @@ public class ManageUser extends VerticalLayout {
 
 		binder.forField(userType)
 				.asRequired()
-				.bind(ManageUserController::getUserType, ManageUserController::setUserType);
+				.bind(User::getUserType, User::setUserType);
 
 		TextField email = new TextField();
 		email.setLabel("Email");
 
 		binder.forField(email)
 				.asRequired()
-				.bind(ManageUserController::getEmail, ManageUserController::setEmail);
+				.bind(User::getEmail, User::setEmail);
 
 		TextField UserId = new TextField();
 		UserId.setLabel("UserId");
 
 		binder.forField(UserId)
 				.asRequired()
-				.bind(ManageUserController::getUserId, ManageUserController::setUserId);
+				.bind(User::getUserID, User::setUserID);
 
 		TextField field = new TextField();
 		field.setLabel("field");
 
 		binder.forField(field)
 				.asRequired()
-				.bind(ManageUserController::getField, ManageUserController::setField);
+				.bind(User::getField, User::setField);
 		
 		form.add(UserId,userName,password,userType, field,email);
 		
@@ -138,7 +139,14 @@ public class ManageUser extends VerticalLayout {
 		//Allow admin to select multiple objects from the grid
 		userGrid.setSelectionMode(SelectionMode.SINGLE);
 
-
+		userGrid.addSelectionListener(e ->{
+			User select = e.getFirstSelectedItem().orElse(null);
+			if(select == null){
+				select = new User(null, null, null, null, null, null);
+			}else{
+				binder.readBean(select);
+			}
+		});
 
 
 		//Label for adding new users.
@@ -155,7 +163,7 @@ public class ManageUser extends VerticalLayout {
 				Dialog invalid = new Dialog();
 				invalid.add(new Label("Please fill out all of the fields"));
 				invalid.open();
-				UI.getCurrent().getPage().reload();
+				userGrid.getDataProvider().refreshAll();
 			}
 			else{
 				HashMap<String, User> addNewUser = new HashMap<String, User>();
@@ -177,10 +185,29 @@ public class ManageUser extends VerticalLayout {
 			}
 		});
 
+		//Edit the user data
+		ArrayList<User> finalUserList = userList;
 		Button edit =  new Button("Edit User", e ->{
 			Set<User> select = userGrid.getSelectedItems();
 			User[] selectedInfo = new User [1];
-			select.toArray();
+			select.toArray(selectedInfo);
+
+			try {
+				binder.writeBean(selectedInfo[0]);
+				HashMap <String, User> selected = new HashMap<String, User>();
+				for(User temp: finalUserList){
+					selected.put(temp.getUserID(), temp);
+				}
+				try {
+					JsonModel.setUserData(selected);
+				}catch(IOException e5){
+					e5.printStackTrace();
+				}
+			}catch(ValidationException e4){
+				e4.printStackTrace();
+			}
+			edited.open();
+			userGrid.getDataProvider().refreshAll();
 		});
 
 		//Button to remove a user.
